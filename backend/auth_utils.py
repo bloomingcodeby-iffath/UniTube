15246f4e65b4584b2e5185630d6d5085b71a4083
Fariba Diya Ahmed <156700868+Fariba-Diya-Ahmed@users.noreply.github.com>
2026-07-08 20:00:15 +0600

    Load JWT configuration from environment variables
    Refactor auth_utils.py to load configuration from environment variables instead of hardcoded values.

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
+
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 
+load_dotenv()
 
-
-SECRET_KEY = "unitube_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+)
 
 
 # =====================
@@ -16,18 +21,15 @@ def create_access_token(data: dict):
 
     to_encode = data.copy()
 
-
     expire = datetime.utcnow() + timedelta(
         minutes=ACCESS_TOKEN_EXPIRE_MINUTES
     )
-
 
     to_encode.update(
         {
             "exp": expire
         }
     )
-
 
     token = jwt.encode(
         to_encode,
